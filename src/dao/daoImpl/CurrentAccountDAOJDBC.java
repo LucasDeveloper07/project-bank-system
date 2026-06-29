4a36456ebb35f7b88f837e5ccd56dbeff84040ab
Lucas Felipe <lucas.repensando435@gmail.com>
2026-06-29 18:33:48 -0300

    CurrentAccount e User DAO

---
+++ src/dao/daoImpl/CurrentAccountDAOJDBC.java
@@ -0,0 +1,306 @@
+package dao.daoImpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import dao.CurrentAccountDAO;
+import dao.UserDAO;
+import dao.db.DAOFactory;
+import dao.db.DB;
+import dao.db.DbException;
+import entities.Account;
+import entities.CurrentAccount;
+import entities.SavingsAccount;
+import exceptions.UserException;
+
+public class CurrentAccountDAOJDBC implements CurrentAccountDAO {
+
+    private Connection conn = null;
+
+    // Construtor para iniciar a classe com a conexão com o banco de dados já criada
+    public CurrentAccountDAOJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    // Método para inserir a currentAccount criada no banco de dados
+    @Override
+    public void insert(CurrentAccount currentAccount) {
+        PreparedStatement st = null;
+        
+        try {
+            st = conn.prepareStatement("INSERT INTO current_account "
+                + "(num, agency, balance, transfer_key, creation_date, maintenance_date, user_id) "
+                + "VALUES "
+                + "(?, ?, ?, ?, ?, ?, ?)");
+
+            st.setString(1, currentAccount.getNum());
+            st.setString(2, currentAccount.getAgencyNum());
+            st.setDouble(3, currentAccount.getBalance());
+            st.setInt(4, currentAccount.getTransferKey());
+            st.setDate(5, java.sql.Date.valueOf(currentAccount.getCreationDate()));
+            st.setDate(6, java.sql.Date.valueOf(currentAccount.getMaintenanceDate()));
+            st.setInt(7, currentAccount.getUser().getId());
+
+            int rowsAffect = st.executeUpdate();
+
+            if (rowsAffect == 0) {
+                throw new DbException("Erro inesperado! Nenhuma linha foi atualizada.");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    // Método para atualizar o saldo da conta após as operações bancárias (DEPOSIT, WITHDRAW, TRANSFER, INTEREST_CREDIT, MAINTENANCE_FEE)
+    @Override
+    public void update(CurrentAccount currentAccount) {
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement("UPDATE current_account "
+                + "SET balance = ? "
+                + "WHERE num = ?");
+
+            st.setDouble(1, currentAccount.getBalance());
+            st.setString(2, currentAccount.getNum());
+
+            int rowsAffect = st.executeUpdate();
+
+            if (rowsAffect == 0) {
+                throw new DbException("Erro inesperado! Nenhuma linha foi atualizada.");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    // Método para deletar a conta do banco de dados após o usuário encerrar a sua conta
+    @Override
+    public void delete(CurrentAccount currentAccount, String password) {
+        // Verificação da senha do usuário para confirmar a operação
+        if (!password.equals(currentAccount.getUser().getPassword())) {
+            throw new UserException("Senha incorreta!");
+        }
+        
+        PreparedStatement st = null;
+
+        try {
+            conn.setAutoCommit(false); // Controle manual da transação para manter a integridade do banco de dados
+
+            // Chamada da operação delete da classe UserDAO para apagar o usuário
+            UserDAO userDao = DAOFactory.createUserDAO();
+
+            userDao.delete(currentAccount.getUser());
+
+            // Operação para apagar a currentAccount
+            st = conn.prepareStatement("DELETE FROM current_account "
+                + "WHERE num = ?");
+
+            st.setString(1, currentAccount.getNum());
+
+            int rowsAffect = st.executeUpdate();
+
+            if (rowsAffect == 0) {
+                throw new DbException("Erro inesperado! Nenhuma linha foi atualizada.");
+            }
+
+            conn.commit(); // Chamada do método para confirmar a operação
+        } catch (SQLException e) {
+            if (conn != null) {
+                try {
+                    conn.rollback(); // Em caso de exceção, o rollback desfaz todas as operações
+                } catch (SQLException e1) {
+                    throw new DbException(e1.getMessage());
+                }
+            }
+
+            throw new DbException(e.getMessage());
+        } catch (DbException e1) {
+            if (conn != null) {
+                try {
+                    conn.rollback(); // Em caso de exceção, o rollback desfaz todas as operações
+                } catch (SQLException e2) {
+                    throw new DbException(e1.getMessage());
+                }
+            }
+
+            throw new DbException(e1.getMessage());
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.setAutoCommit(true);
+                } catch (SQLException e) {
+                    throw new DbException(e.getMessage());
+                }
+            }
+
+            DB.closeStatement(st);
+        }
+    }
+
+    // Método para buscar currentAccount pelo id do usuário (Este método é usado no login para iniciar a conta do usuário)
+    @Override
+    public CurrentAccount findByUserId(int id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT * FROM current_account "
+                + "WHERE user_id = ?");
+
+            st.setInt(1, id);
+
+            rs = st.executeQuery();
+
+            CurrentAccount account = createCurrentAccount(rs);
+
+            return account;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    // Método para buscar currentAccount pela chave de transferência (Usado na operação bancária TRANSFER)
+    @Override
+    public CurrentAccount findByTransferKey(int transferKey) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT * FROM current_account "
+                + "WHERE transfer_key = ?");
+
+            st.setInt(1, transferKey);
+
+            rs = st.executeQuery();
+
+            CurrentAccount account = createCurrentAccount(rs);
+
+            return account;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    // Método para realizar a transferência entre contas
+    @Override
+    public void transfer(CurrentAccount originAccount, Account destinationAccount, double value) {
+        PreparedStatement st = null;
+
+        try {
+            conn.setAutoCommit(false);  // Controle manual da transação para manter a integridade do banco de dados
+
+            // Retirada do saldo da conta de origem
+            st = conn.prepareStatement("UPDATE current_account "
+                + "SET balance = ? "
+                + "WHERE num = ?");
+
+            st.setDouble(1, originAccount.getBalance() - value);
+            st.setString(2, originAccount.getNum());
+
+            int rowsAffect = st.executeUpdate();
+
+            if (rowsAffect == 0) {
+                throw new DbException("Erro inesperado! Nenhuma linha foi atualizada.");
+            }
+
+            DB.closeStatement(st);
+
+            // Verificação para saber a instância da conta destino e buscar no banco de dados
+            if (destinationAccount instanceof CurrentAccount) {
+                st = conn.prepareStatement("UPDATE current_account "
+                    + "SET balance = ? "
+                    + "WHERE num = ?");
+
+                st.setDouble(1, destinationAccount.getBalance() + value);
+                st.setString(2, destinationAccount.getNum());
+
+                rowsAffect = st.executeUpdate();
+
+                if (rowsAffect == 0) {
+                    throw new DbException("Erro inesperado! Nenhuma linha foi atualizada.");
+                }
+            } else if (destinationAccount instanceof SavingsAccount) {
+                st = conn.prepareStatement("UPDATE savings_account "
+                    + "SET balance = ? "
+                    + "WHERE num = ?");
+
+                st.setDouble(1, destinationAccount.getBalance() + value);
+                st.setString(2, destinationAccount.getNum());
+
+                rowsAffect = st.executeUpdate();
+
+                if (rowsAffect == 0) {
+                    throw new DbException("Erro inesperado! Nenhuma linha foi atualizada.");
+                }
+            } else {
+                throw new DbException("Erro de transferência!");
+            }
+
+            conn.commit(); // Chamada do método para confirmar a operação
+        } catch (SQLException e) {
+            if (conn != null) {
+                try {
+                    conn.rollback(); // Em caso de exceção, o rollback desfaz todas as operações
+                } catch (SQLException e1) {
+                    throw new DbException(e1.getMessage());
+                }
+            }
+
+            throw new DbException(e.getMessage());
+        } catch (DbException e1) {
+            if (conn != null) {
+                try {
+                    conn.rollback(); // Em caso de exceção, o rollback desfaz todas as operações
+                } catch (SQLException e2) {
+                    throw new DbException(e1.getMessage());
+                }
+            }
+
+            throw new DbException(e1.getMessage());
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.setAutoCommit(true);
+                } catch (SQLException e) {
+                    throw new DbException(e.getMessage());
+                }
+            }
+
+            DB.closeStatement(st);
+        }
+    }
+
+    // Método privado para criar a currentAccount através do ResultSet
+    private CurrentAccount createCurrentAccount(ResultSet rs) throws SQLException {
+        CurrentAccount account = null;
+
+        if (rs.next()) {
+            String num = rs.getString(1);
+            String agencyNum = rs.getString(2);
+            Double balance = rs.getDouble(3);
+            Integer transferKey = rs.getInt(4);
+            LocalDate creationDate = rs.getDate(5).toLocalDate();
+            LocalDate maintenanceDate = rs.getDate(6).toLocalDate();
+            
+            account = new CurrentAccount(num, agencyNum, balance, creationDate, transferKey, maintenanceDate);
+
+            return account;
+        }
+
+        return null;
+    }
+}
--- src/dao/daoImpl/UserDAOJDBC.java
+++ src/dao/daoImpl/UserDAOJDBC.java
@@ -7,11 +7,15 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 
+import dao.CurrentAccountDAO;
 import dao.UserDAO;
+import dao.db.DAOFactory;
 import dao.db.DB;
 import dao.db.DbException;
 import entities.ClientPf;
 import entities.ClientPj;
+import entities.CurrentAccount;
+import entities.SavingsAccount;
 import entities.User;
 import exceptions.UserException;
 
@@ -19,34 +23,52 @@ public class UserDAOJDBC implements UserDAO {
 
     private Connection conn = null;
 
+    // Construtor para iniciar a classe com a conexão com o banco de dados já criada
     public UserDAOJDBC(Connection conn) {
         this.conn = conn;
     }
 
+    // Método para inserir user criado no banco de dados
     @Override
     public void insert(User user) {
         PreparedStatement st = null;
         
         try {
             st = conn.prepareStatement("INSERT INTO user " 
-                + "(name, email, password, birth_date, type, cpf, cnpj) "
+                + "(name, email, password, birth_date, type, type_account, cpf, cnpj) "
                 + "VALUES "
-                + "(?, ?, ?, ?, ?, ?, ?)",
+                + "(?, ?, ?, ?, ?, ?, ?, ?)",
                 Statement.RETURN_GENERATED_KEYS);
+
+            // A coluna type_account no banco, auxilia na busca da conta quando o usuário fizer o login 
 
             st.setString(1, user.getName());
             st.setString(2, user.getEmail());
             st.setString(3, user.getPassword());
             st.setDate(4, java.sql.Date.valueOf(user.getBirthDate()));
 
+            // Verificação para saber a instância de User
             if (user instanceof ClientPf clientPf) {
                 st.setString(5, "PF");
-                st.setString(6, clientPf.getCpf());
-                st.setString(7, null);
+                st.setString(7, clientPf.getCpf());
+                st.setString(8, null);
+
+                // Verificação para saber a instância de Account
+                if (clientPf.getAccount() instanceof CurrentAccount) {
+                    st.setString(6, "CURRENT");
+                } else if (clientPf.getAccount() instanceof SavingsAccount) {
+                    st.setString(6, "SAVINGS");
+                }
             } else if (user instanceof ClientPj clientPj) {
                 st.setString(5, "PJ");
-                st.setString(7, clientPj.getCnpj());
-                st.setString(6, null);
+                st.setString(8, clientPj.getCnpj());
+                st.setString(7, null);
+
+                if (clientPj.getAccount() instanceof CurrentAccount) {
+                    st.setString(6, "CURRENT");
+                } else if (clientPj.getAccount() instanceof SavingsAccount) {
+                    st.setString(6, "SAVINGS");
+                }
             }
             
             int rowsAffect = st.executeUpdate();
@@ -56,11 +78,10 @@ public class UserDAOJDBC implements UserDAO {
 
                 if (rs.next()) {
                     int id = rs.getInt(1);
-                    user.setId(id);
+                    user.setId(id); // Inserção da chave primária de user na instância
                 }
             } else {
                 throw new DbException("Erro inesperado! Nenhuma linha foi atualizada.");
-
             }
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
@@ -69,7 +90,7 @@ public class UserDAOJDBC implements UserDAO {
         }
     }
 
-
+    // Método para atualizar a senha do usuário
     @Override
     public void updatePassword(User user) {
         PreparedStatement st = null;
@@ -94,7 +115,7 @@ public class UserDAOJDBC implements UserDAO {
         }
     }
 
-    
+    // Método para atualizar o nome do usuário
     @Override
     public void updateName(User user) {
         PreparedStatement st = null;
@@ -119,6 +140,7 @@ public class UserDAOJDBC implements UserDAO {
         }
     }
 
+    // Método para deletar User do banco de dados
     @Override
     public void delete(User user) {
         PreparedStatement st = null;
@@ -141,63 +163,58 @@ public class UserDAOJDBC implements UserDAO {
         }
     }
 
+    // Método para realizar login do user
     @Override
     public User login(String cpf_cnpj, String email, String password) {
         PreparedStatement st = null;
         ResultSet rs = null;
 
         try {
-            st = conn.prepareStatement("SELECT email, cpf, cnpj, password FROM user "
+            st = conn.prepareStatement("SELECT * FROM user "
                 + "WHERE email = ?");
 
             st.setString(1, email);
 
             rs = st.executeQuery();
 
+            // Condição para verificar o email
             if (rs.next()) {
-                String verifDate;
-                boolean verifCpf;
+                String verifData; // Variável para realizar a verificação dos dados de login
+                boolean verifCpf; // Variável para saber se é cpf ou cnpj
     
                 if (cpf_cnpj.length() == 11) {
-                    verifDate = rs.getString(2);
+                    verifData = rs.getString(8);
                     verifCpf = true;
     
-                    if (!cpf_cnpj.equals(verifDate)) {
+                    // Verifição do CPF informado pelo usuário
+                    if (!cpf_cnpj.equals(verifData)) {
                         throw new UserException("Usuário ou senha inválidos");
                     }
                 } else if (cpf_cnpj.length() == 14) {
-                    verifDate = rs.getString(3);
+                    verifData = rs.getString(9);
                     verifCpf = false;
                     
-                    if (!cpf_cnpj.equals(verifDate)) {
+                    // Verificação do CNPJ informado pelo usuário
+                    if (!cpf_cnpj.equals(verifData)) {
                         throw new UserException("Usuário ou senha inválidos");
                     }
                 } else {
-                    throw new UserException("Erro! CPF ou CNPJ inválido.");
+                    throw new UserException("Erro! CPF ou CNPJ inválido."); 
                 }
                 
-                verifDate = rs.getString(4);
+                verifData = rs.getString(4);
                 
-                if (!password.equals(verifDate)) {
+                // Verificação da senha informada pelo usuário
+                if (!password.equals(verifData)) {
                     throw new UserException("Usuário ou senha inválidos");
                 }
-    
-                st = conn.prepareStatement("SELECT * FROM user "
-                    + "WHERE email = ?");
-    
-                st.setString(1, email);
-    
-                rs = st.executeQuery();
-                
-                if (rs.next()) {
-                    User user = createUser(rs, verifCpf);
-                    
-                    return user;
-                }
 
-                return null;
+                // Chamada do método para instanciar usuário após a verificação dos dados
+                User user = createUser(rs, verifCpf);
+                
+                return user;
             } else {
-                throw new UserException("Usuário ou senha inválidos!");
+                throw new UserException("Usuário ou senha inválidos!"); // Lançamento de exceção caso o email esteja inválido
             }
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
@@ -207,33 +224,46 @@ public class UserDAOJDBC implements UserDAO {
         }
     }
 
+    // Método privado para criar usuário através do ResultSet e da variável verifCpf
     private User createUser(ResultSet rs, boolean verifCpf) throws SQLException {
         User user = null;
 
+        // Instânciação do user com CPF
         if (verifCpf) {
             int id = rs.getInt(1);
             String name = rs.getString(2);
             String email = rs.getString(3);
             String password = rs.getString(4);
             LocalDate birthDate = rs.getDate(5).toLocalDate();
-            Long cpf = rs.getLong(7);
+            Long cpf = rs.getLong(8);
+            String typeAccount = rs.getString(7);
 
-            user = new ClientPf(name, email, password, birthDate, cpf);
-            user.setId(id);
+            // Verificação do tipo de conta do usuário para realizar a busca no banco de dados
+            if (typeAccount.equals("CURRENT")) {
+                CurrentAccountDAO currentDao = DAOFactory.createCurrentAccountDAO();
 
-            return user;
+                // Chamada do construtor de ClientPf passando os atributos e a chamada do método findByUserId de CurrentAccount que retorna uma CurrentAccount
+                user = new ClientPf(name, email, password, birthDate, cpf, currentDao.findByUserId(id));
+                user.setId(id);
+
+                return user;
+            }
+
         } else {
+            // Instânciação do user com CNPJ
             int id = rs.getInt(1);
             String name = rs.getString(2);
             String email = rs.getString(3);
             String password = rs.getString(4);
             LocalDate birthDate = rs.getDate(5).toLocalDate();
-            Long cnpj = rs.getLong(8);
+            Long cnpj = rs.getLong(9);
 
-            user = new ClientPf(name, email, password, birthDate, cnpj);
+            user = new ClientPj(name, email, password, birthDate, cnpj);
             user.setId(id);
 
             return user;
         }
+
+        return null;
     }
 }
